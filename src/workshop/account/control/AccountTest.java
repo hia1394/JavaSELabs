730818c60b163d9ceb905540bdb685921258a02b
hia1394 <a4737465@naver.com>
2025-08-06 14:37:11 +0900

    .

---
+++ src/workshop/account/control/AccountTest.java
@@ -0,0 +1,13 @@
+package workshop.account.control;
+
+import workshop.account.entity.Account;
+
+public class AccountTest {
+		public static void main(String[] args) {
+			Account account = new Account();
+			
+			account.setCustId("A1100");
+			account.setAcctId("221-22-3477");
+
+		}
+}
--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,9 +1,53 @@
 package workshop.account.entity;
 
 public class Account {
-	private String cstId;
+	private String custId;
 	private String acctId;
 	private int balance;
-	
-	
+
+	public Account() {
+		System.out.println("기본생성자 호출");
+	}
+
+	public Account(String custId, String acctId, int balance) {
+
+//		this.custId = custId;
+//		this.acctId = acctId;
+		setCustId(custId);
+		setAcctId(acctId);
+		this.balance = balance;
+	}
+
+//	public void setBalance(int newBalance) {
+//		balance = newBalance;
+//	}
+	private void getbalacne() {
+		return balance;
+	}
+
+	public String getCustId() {
+		return custId;
+	}
+
+	public void setCustId(String cstId) {
+		this.custId = custId;
+	}
+
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+
+	public String getAcctId() {
+		return acctId;
+	}
+
+	// 입금
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+
+	// 출금
+	public void withdraw(int amount) {
+		this.balance -= amount;
+	}
 }
